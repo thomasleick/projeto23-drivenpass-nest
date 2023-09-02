783989a309fa2e52ab3f43c47b3419826b636f37
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-09-02 19:22:05 -0300

    feat: nest generate CRUD notes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CredentialsModule } from './credentials/credentials.module';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [UsersModule, CredentialsModule],
+  imports: [UsersModule, CredentialsModule, NotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
